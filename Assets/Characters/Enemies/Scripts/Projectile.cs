a0b6ff5fc4234c5cf868ad7785e71db331b953d6
Toni Fang <tonifang@189.cn>
2017-06-16 12:36:09 +0800

    retry

---
+++ Assets/Characters/Enemies/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float projectileSpeed; // Note other classed can set
+
+	float damageCaused;
+	public void SetDamage (float damage) {
+		damageCaused = damage;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Component damageableComponent = collision.gameObject.GetComponent (typeof(IDamageable));
+		if (damageableComponent) {
+			(damageableComponent as IDamageable).TakeDamage (damageCaused);
+		}
+        Destroy(gameObject, 0.05f);
+	}
+}
